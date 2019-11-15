a83026cf62502564b58ea45a2bfa25fa4d3ea911
Chase Redford <chaseredford@Chases-MacBook-Pro.local>
2019-11-15 11:37:39 -0700

    NEED TO STYLE

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+	return (
+		<div className="cards">
+			<div>
+				<img src={props.character.image} />
+				<div>
+					<p>Name: {props.character.name}</p>
+					<p>Gender: {props.character.gender}</p>
+					<p>Species: {props.character.species}</p>
+					<p>Origin: {props.character.origin.name}</p>
+					<p>Created: {props.character.created}</p>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,39 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+	// TODO: Add useState to track data from useEffect
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+	const [ char, setChar ] = useState([]);
+	const [ filtered, setFiltered ] = useState([]);
 
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+	const search = (array) => {
+		setFiltered(array);
+	};
+
+	useEffect(() => {
+		// TODO: Add API Request here - must run in `useEffect`
+		//  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+		axios.get("https://rickandmortyapi.com/api/character/").then((response) => {
+			console.log(response.data.results);
+			setChar(response.data.results);
+			setFiltered(response.data.results);
+		});
+	}, []);
+
+	return (
+		<section className="character-list">
+			<h2>Characters</h2>
+			<Link className="main__1" to={"/"}>
+				Home
+			</Link>
+			<SearchForm search={search} character={char} />
+			{filtered.map((character) => {
+				return <CharacterCard key={character.id} character={character} />;
+			})}
+		</section>
+	);
 }
